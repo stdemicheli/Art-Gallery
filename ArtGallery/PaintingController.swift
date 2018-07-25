54765634df6a130da62be8efdbeffa327819eb79
stdemicheli <stdemicheli@gmail.com>
2018-07-25 22:28:39 +0200

    Add PaintingController

---
+++ ArtGallery/PaintingController.swift
@@ -0,0 +1,33 @@
+//
+//  PaintingController.swift
+//  ArtGallery
+//
+//  Created by De MicheliStefano on 25.07.18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PaintingController {
+    
+    func loadPaintingsFromAssets() {
+        for index in 1...12 {
+            let name = "Image\(index)"
+            guard let file = UIImage(named: name) else { return }
+            let painting = Painting(image: file)
+            paintings.append(painting)
+        }
+    }
+    
+    func toggleIsLiked(for painting: Painting) {
+        painting.isLiked = !painting.isLiked
+    }
+    
+    //MARK: - Properties
+    private(set) var paintings: [Painting] = []
+    
+    init() {
+        loadPaintingsFromAssets()
+    }
+}
